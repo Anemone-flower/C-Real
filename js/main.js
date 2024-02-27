1856bf81d3d8ecf711610e747de2f30dd66aed46
Anemone-flower <129704443+Anemone-flower@users.noreply.github.com>
2024-02-28 00:05:10 +0900

    02-28

--- js/main.js
+++ js/main.js
@@ -8,5 +8,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+const C_KEY_CODE = 67;
 
+function openTextInput() {
+    const userInput = alert("C,REAL 이벤트 힌트 : 1호관 계단 어딘가.");
+}
 
+function keyPressHandler(event) {
+    if (event.keyCode === C_KEY_CODE) {
+        openTextInput();
+    }
+}
+
+document.addEventListener("keydown", keyPressHandler);
